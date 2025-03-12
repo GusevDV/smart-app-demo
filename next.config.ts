a2af0a4a6c2cf7f7a992dea89e0ac5348d4e4334
Ilia Polozov <IKPolozov@beeline.ru>
2025-03-12 11:26:30 +0300

    [AIC-1] - add rewrites proxy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/',
+        destination: `${process.env.EXTERNAL_API_DOMAIN}/mobile/api/`,
+      },
+    ]
+  },
   /* config options here */
 };
 
