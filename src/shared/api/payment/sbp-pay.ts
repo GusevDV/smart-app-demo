eae2bf9e2733ccaf074f9afe527699d62d22376c
Dmitry Gusev <dvgusev@hotmail.com>
2025-03-13 09:08:42 +0300

    Fix ctn

--- src/shared/api/payment/sbp-pay.ts
+++ src/shared/api/payment/sbp-pay.ts
@@ -1,6 +1,9 @@
+'use client'
+
 import { useMutation } from "@tanstack/react-query";
 import { httpClient } from "../baseQuery";
 import { AxiosError } from "axios";
+import { getAuthData } from "@/shared/lib/auth";
 
 export type SbpPayRequest = {
   amount: number;
@@ -23,8 +26,12 @@ export type SbpPayResponse = {
 }
 
 async function sbpPay(request: SbpPayRequest): Promise<SbpPayResponse> {
+  // TODO удалить после перехода на cookie
+  const authData = getAuthData();
+  const ctn = authData?.ctn ?? '';
+
   try {
-    const response = await httpClient.post<SbpPayResponse>("/v2/payment/sbpPay?ctn=${xCtn}", {
+    const response = await httpClient.post<SbpPayResponse>(`/v2/payment/sbpPay?ctn=${ctn}`, {
       amount: request.amount
     });
     return response.data;
